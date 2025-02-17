1d3ca2e8ab56660a6b013b46d76309b5998d20a3
Mihai Adrian Andrei <mihai.andrei1597@gmail.com>
2025-02-17 10:32:46 +0200

    add tailwind v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
 import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
+      <div className="bg-red-500">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
