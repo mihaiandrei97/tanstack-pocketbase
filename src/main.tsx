14fda69ccd734e21a8e7fc30915f9348585b6c08
Mihai Adrian Andrei <mihai.andrei1597@gmail.com>
2025-02-17 10:41:51 +0200

    added tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+
+import ReactDOM from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+// Create a new router instance
+const router = createRouter({ routeTree });
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
     tailwindcss(),
     react(),
   ],
